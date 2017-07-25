7211f20636698ae0ee35c7b951c2185df129df2f
too-young-too-naive <jinliang_guo@outlook.com>
2017-07-25 15:10:42 -0700

    updated

--- script.js
+++ script.js
@@ -4,24 +4,25 @@ app = angular.module('app', ['angucomplete-alt']);
 
 app.controller('AppCtrl', function($scope, $http) {
 
-  $scope.names = [
-    {name: 'Alex', age: 30},
-    {name: 'John', age: 20},
-    {name: 'Tim robin', age: 25},
-    {name: 'Tim', age: 25},
-    {name: 'John ', age: 25},
-    {name: 'John Smith', age: 25},
-    {name: 'Anthony', age: 25}
-    ];
-
-  $scope.focusIn = function() {
-    $scope.message = 'focus in';
-  };
-  $scope.focusOut = function() {
-    $scope.message = 'focus out';
-  };
   $scope.click = function () {
-      $scope.show = 'haha';
-      console.log('hahahahaha');
+    console.log($scope.testObj.title);
+    var searchStr = $scope.testObj.title;
+    $http({
+        method: 'GET',
+        url: 'http://10.96.97.227:8086/kg/?' + searchStr
+    }).success(function successCallback(res) {
+        console.log('Success call back.');
+        console.log(res);
+        if(res.records.length == 0){
+          $scope.results = 'NO RESULTS!';
+        } else {
+            $scope.isTrue = true;
+            $scope.results = res.records;
+        }
+
+    }, function errorCallback(err) {
+        console.log('Error call back');
+        console.log(err);
+    })
   };
 });
