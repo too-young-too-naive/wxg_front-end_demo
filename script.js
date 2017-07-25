b511d019f2e3cd2956f18bda673c4adee13bc10f
jinliang_guo <jinliang_guo@outlook.com>
2017-07-25 09:58:16 -0700

    version1.0

---
+++ script.js
@@ -0,0 +1,26 @@
+// Code goes here
+
+app = angular.module('app', ['angucomplete-alt']);
+
+app.controller('AppCtrl', function($scope) {
+
+  $scope.names = [
+    {name: 'Alex', age: 30},
+    {name: 'John', age: 20},
+    {name: 'Tim robin', age: 25},
+    {name: 'Tim', age: 25},
+    {name: 'John ', age: 25},
+    {name: 'John Smith', age: 25},
+    {name: 'Anthony', age: 25}
+    ];
+
+  $scope.focusIn = function() {
+    $scope.message = 'focus in';
+  };
+  $scope.focusOut = function() {
+    $scope.message = 'focus out';
+  };
+  $scope.click = function () {
+      $scope.show = 'haha';
+  }
+});
